be7d169c219487cf5e28bcad4cd3dd001cf3f4b8
Muhammad Shahbaz <M023Shah@gmail.com>
2022-08-09 01:59:21 -0700

    creating skeleton

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import NavBar from "./navbar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+
+      < NavBar />
+      <h1>
+        The Daily Rana
+      </h1>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./navbar.css"
+
+function NavBar() {
+
+    return (
+
+    <div
+    className='nav'
+    >
+        <div className="topLeft"> left </div>
+        <div className="topCenter"> Center </div>
+        <div className="topRight"> Right </div>
+    </div>
+
+    );
+}
+
+export default NavBar;
