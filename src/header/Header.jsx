07e2d29df47cebebc99c1aa2ef8d0dbaa6572803
Muhammad Shahbaz <M023Shah@gmail.com>
2022-08-11 00:37:57 -0700

    adding homepage

---
+++ src/header/Header.jsx
@@ -0,0 +1,23 @@
+import "./header.css"
+
+const Header = () => {
+
+
+    return (
+
+    <div
+    className="header"
+    >
+        <div className="headerTitles" > 
+            <span className="headerTitleSm" > React and Node </span>
+            <span className="headerTitleLg" > Blog </span>
+            
+        </div>
+        <img className="headerImg" src="https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+        
+    </div>
+
+    )
+}
+
+export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import "./home.css"
+import Header from "../../header/Header"
+
+const Home = () => {
+
+
+return (
+
+    <div
+    className="home"
+    >
+        <Header />
+        Homepage
+    </div>
+
+    )
+}
+
+export default Home
