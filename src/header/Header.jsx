9ecd2960d2633cbbcfe2707539105b337aabe91e
Muhammad Shahbaz <M023Shah@gmail.com>
2022-08-14 17:49:34 -0700

    homepage work

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
     className="header"
     >
         <div className="headerTitles" > 
-            <span className="headerTitleSm" > React and Node </span>
-            <span className="headerTitleLg" > Blog </span>
+            <span className="headerTitleSm" > </span>
+            <span className="headerTitleLg" > The Daily Rana </span>
             
         </div>
         <img className="headerImg" src="https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -18,7 +18,32 @@ const Sidebar = () => {
         </div>
 
         <div className="sidebarItem" >  
-            <span> CATEGORIES </span>
+            <span className="sidebarTitle" > CATEGORIES </span>
+
+            <ul className="sidebarList">
+
+                <li className="sidebarListItem" >Life</li>
+                <li className="sidebarListItem" >Music</li>
+                <li className="sidebarListItem" >Style</li>
+                <li className="sidebarListItem" >Sport</li>
+                <li className="sidebarListItem" >Tech</li>
+                <li className="sidebarListItem" >Cinema</li>
+                
+            </ul>
+        </div>
+        <div className="sidebarItem" >
+
+            <span className="sidebarTitle"> FOLLOW US </span>
+
+            <div className="sidebarSocial" >
+
+            <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+
+            </div>
+
         </div>
 
     </div>
